04ca85fa0f8ee722ae60696b7f65921d84a2625d
Kais <kais.hammami@esprit.tn>
2024-04-28 11:28:01 +0100

    bundles (excel+drive)

--- src/Services/UserExcelExporter.php
+++ src/Services/UserExcelExporter.php
@@ -1,7 +1,11 @@
 <?php
 
 namespace App\Service;
+require_once __DIR__ . '/../vendor/autoload.php';
 
+use Google\Service\Drive;
+use Google\Client as GoogleClient;
+use Google\Service\Drive\DriveFile;
 use PhpOffice\PhpSpreadsheet\Spreadsheet; // Importez la classe Spreadsheet
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use App\Entity\Utilisateur;
@@ -51,5 +55,37 @@ class UserExcelExporter
         // Enregistre le fichier Excel
         $writer = new Xlsx($spreadsheet);
         $writer->save($filePath);
+
+        
+// Initialiser le client Google
+$client = new GoogleClient();
+$client->setApplicationName('VOLTRIDE_SYM');
+$client->setClientId('321041928925-nrbors4kbmdtgaaj6kitck6ppb4gj6av.apps.googleusercontent.com'); // Remplacez VOTRE_CLIENT_ID par votre identifiant client
+$client->setClientSecret('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'); // Remplacez VOTRE_CLIENT_SECRET par votre client secret
+$client->addScope(Drive::DRIVE_FILE);
+$client->setAccessType('offline');
+$client->setRedirectUri('https://127.0.0.1:8000/oauth2callback.php'); // Assurez-vous de remplacer par l'URI de redirection correcte
+
+// Vérifier si le code d'autorisation est présent dans la requête
+if (isset($_GET['code'])) {
+    // Échanger le code d'autorisation contre un jeton d'accès
+    $client->fetchAccessTokenWithAuthCode($_GET['code']);
+
+    // Récupérer le jeton d'accès
+    $accessToken = $client->getAccessToken();
+
+    // Stocker le jeton d'accès dans la session ou la base de données, etc.
+    $_SESSION['access_token'] = $accessToken;
+
+    // Rediriger vers une page de votre application après l'authentification réussie
+    header('Location: https://127.0.0.1:8000/oauth2callback.php'); // Remplacez par l'URL de votre application
+    exit();
+} else {
+    // Si le code d'autorisation n'est pas présent, rediriger vers l'URL d'autorisation OAuth
+    $authUrl = $client->createAuthUrl();
+    header('Location: ' . $authUrl);
+    exit();
+}
+        
     }
 }
